e4e72fbdb736900c331a392c8c2fc18e9a77abf5
loulou123546 <louis.maillard@protonmail.com>
2023-12-07 00:01:56 +0100

    feat: parse more in-depth details

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,9 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ThreatModelV2 {
-    pub summary: Summary
+    pub summary: Summary,
+    pub detail: Detail,
+    pub version: String
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -10,5 +12,32 @@ pub struct Summary {
     pub title: Option<String>,
     pub owner: Option<String>,
     pub description: Option<String>,
+    pub id: Option<usize>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[allow(non_snake_case)]
+pub struct Detail {
+    pub contributors: Option<Vec<Contributor>>,
+    pub reviewer: Option<String>,
+    pub diagramTop: Option<usize>,
+    pub threatTop: Option<usize>,
+    pub diagrams: Option<Vec<Diagram>>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Contributor {
+    pub name: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[allow(non_snake_case)]
+pub struct Diagram {
+    // pub cells
+    pub version: Option<String>,
+    pub title: Option<String>,
+    pub descrition: Option<String>,
+    pub thumbnail: Option<String>,
+    pub diagramType: String,
     pub id: Option<usize>
 }
--- tests/parsing.rs
+++ tests/parsing.rs
@@ -10,8 +10,40 @@ fn read_file (file: &str) -> threat_dragon_types::ThreatModelV2 {
 #[test]
 pub fn parse_summary () {
     let v2 = read_file("tests/demo_files/Demo_Threat_Model_V2.json");
+
     assert_eq!(v2.summary.title, Some("Demo Threat Model".to_owned()));
     assert_eq!(v2.summary.owner, Some("Mike Goodwin".to_owned()));
     assert_eq!(v2.summary.description, Some("A sample model of a web application, with a queue-decoupled background process.".to_owned()));
     assert_eq!(v2.summary.id, Some(0));
+}
+
+#[test]
+pub fn parse_details () {
+    let v2 = read_file("tests/demo_files/Demo_Threat_Model_V2.json");
+
+    match v2.detail.contributors {
+        Some(contribs) => {
+            assert!(contribs.get(0).is_some_and(|x| x.name == "Tom Brown"));
+            assert!(contribs.get(1).is_some_and(|x| x.name == "Albert Moneypenny"));
+        }
+        None => panic!("Contributors is None while expected to be Tom Brown + Albert Moneypenny")
+    };
+    assert_eq!(v2.detail.reviewer, Some("Jane Smith".to_owned()));
+    assert_eq!(v2.detail.threatTop, Some(0));
+    assert_eq!(v2.detail.diagramTop, Some(0));
+    assert!(v2.detail.diagrams.is_some());
+}
+
+#[test]
+pub fn parse_diagram_info () {
+    let v2 = read_file("tests/demo_files/Demo_Threat_Model_V2.json");
+    let diagrams = v2.detail.diagrams.expect("Found no diagrams");
+    let diagram = diagrams.get(0).expect("Diagram at index 0 does not exist");
+
+    assert_eq!(diagram.version, Some("2.0".to_owned()));
+    assert_eq!(diagram.title, Some("Main Request Data Flow".to_owned()));
+    assert_eq!(diagram.descrition, Some("Main Request Data Flow Description".to_owned()));
+    assert_eq!(diagram.thumbnail, Some("./public/content/images/thumbnail.stride.jpg".to_owned()));
+    assert_eq!(diagram.diagramType, "STRIDE".to_owned());
+    assert_eq!(diagram.id, Some(0));
 }
